33584e1a391fc24b6a9d2c5ebae6a4487e3178cf
Diego Aguiar Medeiros <diego.a.medeiros@gmail.com>
2023-07-22 19:02:47 -0300

    add cors

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,8 @@ import * as dotenv from 'dotenv';
 async function bootstrap() {
   dotenv.config();
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
   app.setGlobalPrefix('/api');
-  await app.listen(3000);
+  await app.listen(3001);
 }
 bootstrap();
